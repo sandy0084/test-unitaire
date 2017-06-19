c77f1fe579bb7998ec3bbda34d4cd3794346b4ad
NOaN <notonlyanerd@gmail.com>
2017-06-19 16:54:21 +0200

    First Unit test

---
+++ src/test/AsciiPrinterTest.java
@@ -0,0 +1,33 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import main.AsciiPrinter;
+
+/**
+ * 
+ */
+
+/**
+ * @author dcyheat
+ *
+ */
+public class AsciiPrinterTest {
+
+    /**
+     * Test method for {@link main.AsciiPrinter#AsciiPrinter()}.
+     */
+    @Test
+    public void testAsciiPrinter() {
+        assertNotNull(new AsciiPrinter());
+    }
+
+    /**
+     * Test method for {@link main.AsciiPrinter#print(java.lang.String, int)}.
+     */
+    @Test
+    public void testPrint() {
+        fail("Not yet implemented");
+    }
+
+}
