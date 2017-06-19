73a58dc548a0bd433e39a6319e1d6d147843fc40
NOaN <notonlyanerd@gmail.com>
2017-06-19 14:39:29 +0200

    Adding Eclipse project

---
+++ src/core/main/Launcher.java
@@ -0,0 +1,19 @@
+package main;
+
+/**
+ * @author NOaN
+ *
+ */
+public class Launcher {
+
+    /**
+     * Main entry point
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
